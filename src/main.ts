f4f90e8b68bfb76289fcfa229310ffbdd7ce269b
Wil <joswilder46@gmail.com>
2021-07-08 10:25:20 -0300

    expose port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, function () {
+    console.log(
+      'Express server listening on port %d in %s mode',
+      this.address().port,
+    );
+  });
 }
 bootstrap();
